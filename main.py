59264a3e1a0d8d4c060b82d3d6ca55f9dfd9b76d
Leonardo <102773282+LeonardoJoseDaSilvaJunior@users.noreply.github.com>
2023-04-12 17:05:14 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,3 @@
-primeiraNota = float(input("Insira a primeira nota:"))
-while primeiraNota < 0:
-    print("Nota menor que zero, insira uma nota válid")
-    primeiraNota = float(input("Insira a primeira nota:"))
-    if(primeiraNota > 0):
-        break
-segundaNota = float(input("Insira a Segunda nota:"))
-while segundaNota < 0:
-    print("Nota menor que zero, insira uma nota válid")
-    segundaNota = float(input("Insira a segunda nota:"))
-    if(segundaNota > 0):
-        break
+for i in range (100, 110+1):
+    print(i)
 
-media = (primeiraNota + segundaNota)/2
-
-
-
-print("\n__________BOLETIN_________")
-print("Primeira nota: {0}\nSegunda nota: {1}\nMedia{2}".format(primeiraNota,segundaNota,media))
